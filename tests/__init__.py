f45378056fe43aa11c67ffa45dc4612e2f83b5cc
rebeccabuchholz <rebecca.kurup@wsl.ch>
2025-04-22 18:07:24 +0200

    test: unit tests for exporter, logger, and validators

---
+++ tests/__init__.py
@@ -0,0 +1,8 @@
+"""
+Test suite for datacite-websnap.
+
+Example command that prints test coverage to terminal:
+pdm run pytest --cov=datacite_websnap --cov-report=term-missing
+"""
+
+# TODO write tests for cli.py, datacite_handler.py
